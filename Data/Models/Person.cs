158d553223a734063ccd20bf43734c0e6700882f
sajjadmkb <sajjadmkb@gmail.com>
2025-01-21 21:35:43 +0500

    .

---
+++ Data/Models/Person.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibble.Data.Models
+{
+    public class Person
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+    }
+}
